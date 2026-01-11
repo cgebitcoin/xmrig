6d812226f4da7edc1d10d6c7011af32f0114d023
Gurkan Erdogdu <gurkan.erdogdu@mechsoft.com.tr>
2026-01-11 02:56:57 -0500

    add check

--- src/base/net/stratum/Job.cpp
+++ src/base/net/stratum/Job.cpp
@@ -107,6 +107,20 @@ bool xmrig::Job::setSeedHash(const char *hash) {
 
   m_seed = Cvt::fromHex(hash, kMaxSeedSize * 2);
 
+  // [BitMinti] Native Seed Reversal
+  // Daemon sends Big Endian (RPC). RandomX needs Little Endian bytes.
+  // We reverse here so XMRig is "Aligned".
+  if (!m_seed.empty()) {
+    uint8_t *data = m_seed.data();
+    size_t len = m_seed.size();
+    for (size_t i = 0; i < len / 2; ++i) {
+      uint8_t temp = data[i];
+      data[i] = data[len - 1 - i];
+      data[len - 1 - i] = temp;
+    }
+    printf("[BitMinti Job] Seed Reversed for Native Alignment.\n");
+  }
+
   return !m_seed.empty();
 }
 
